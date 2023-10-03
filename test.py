8dd3163aa9a504d9510c5c4e41cee3d348b49a95
jmvillasan <qjmevillasan@tip.edu.ph>
2023-10-03 16:19:15 +0800

    Commit pytests

---
+++ test.py
@@ -0,0 +1,28 @@
+import unittest
+
+def test_sqrt():
+
+    num=25
+    assert math.sqrt(num) == 5
+
+def test_square():
+
+    num=7
+    assert math.square(num) == 49
+
+def test_check():
+
+    num=100
+    assert x >= 100
+
+def test_multiply():
+
+    x= 4
+    y= 5
+    assert x*y == 20
+
+def test_fun():
+
+    x= 4
+    return x+1
+    assert x == 5
